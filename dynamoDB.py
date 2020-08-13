9e380fa2bf8d300257ca041bb5ba5e1dcbfadb34
Christon Cardoza <Christon.Antony_cardoza@rsa.com>
2020-08-13 18:41:23 +0530

    Added DynamoDB code

---
+++ dynamoDB.py
@@ -0,0 +1,91 @@
+import boto3
+import random
+
+def lambda_handler(event, context):
+    client = boto3.client('dynamodb')
+
+    table_name = 'Employee_Details'
+    # create_table_response = Create_Table(client,table_name)
+    # list_table_response = List_Table(client)
+    # delete_table_response = Delete_Table(client,'Test')
+    # put_item_response = Put_Item(client, table_name)
+    get_iem_response = Get_Item(client, table_name)
+
+def Create_Table(dynamo,tbName):
+    create = dynamo.create_table(
+
+        TableName=tbName,
+        KeySchema=[
+            {
+                'AttributeName': 'Designation',
+                'KeyType': 'HASH'
+            },
+            {
+                'AttributeName': 'Name',
+                'KeyType': 'RANGE'
+            }
+        ],
+        AttributeDefinitions=[
+            {
+                'AttributeName': 'Name',
+                'AttributeType': 'S'
+            },
+            {
+                'AttributeName': 'Designation',
+                'AttributeType': 'S'
+            }
+        ],
+        ProvisionedThroughput={
+            'ReadCapacityUnits': 123,
+            'WriteCapacityUnits': 123
+        }
+    )
+
+    print(create)
+    return create
+
+def List_Table(dynamo):
+    table_list = dynamo.list_tables()
+    print(table_list['TableNames'])
+    return table_list['TableNames']
+
+def Delete_Table(dynamo,tbName):
+    response = dynamo.delete_table(TableName= tbName)
+    print(response)
+    return(response)
+
+def Put_Item(dynamo,tbName):
+    Designation = ['Software Engineer','Software Quality Engineer', 'Security Analyst']
+    Name = ['Christon', 'Wilfred', 'Rohan']
+    SerName = ['Cardoza', 'Fernendes','D\'Souza']
+    response = dynamo.put_item(
+
+        TableName= tbName,
+        Item={
+            'Designation':{'S': random.choice(Designation)} ,
+            'Name':{'S':random.choice(Name)},
+            'SerName':{'S':random.choice(SerName)}
+
+
+        }
+    )
+    print(response)
+    return(response)
+
+def Get_Item(dynamo,tbName):
+    response = client.get_item(
+
+        TableName = tbName,
+
+        Key={
+            'Name': {
+                'S': 'Acme Band',
+            },
+            'SongTitle': {
+                'S': 'Happy Day',
+            },
+        },
+
+    )
+
+    print(response)
