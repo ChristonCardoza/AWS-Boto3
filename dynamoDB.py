b4d1b3b920a26d32cc59e38512817499599cb156
Christon Cardoza <Christoncardoza96@gmail.com>
2020-08-17 11:38:34 +0530

    update items

--- dynamoDB.py
+++ dynamoDB.py
@@ -9,7 +9,10 @@ def lambda_handler(event, context):
     # list_table_response = List_Table(client)
     # delete_table_response = Delete_Table(client,'Test')
     # put_item_response = Put_Item(client, table_name)
-    get_iem_response = Get_Item(client, table_name)
+    # get_item_response = Get_Item(client, table_name)
+    # delete_item_response = Delete_Item(client, table_name)
+    update_item_response = Update_Item(client, table_name)
+    
 
 def Create_Table(dynamo,tbName):
     create = dynamo.create_table(
@@ -17,11 +20,11 @@ def Create_Table(dynamo,tbName):
         TableName=tbName,
         KeySchema=[
             {
-                'AttributeName': 'Designation',
+                'AttributeName': 'Name',
                 'KeyType': 'HASH'
             },
             {
-                'AttributeName': 'Name',
+                'AttributeName': 'Surname',
                 'KeyType': 'RANGE'
             }
         ],
@@ -31,7 +34,7 @@ def Create_Table(dynamo,tbName):
                 'AttributeType': 'S'
             },
             {
-                'AttributeName': 'Designation',
+                'AttributeName': 'Surname',
                 'AttributeType': 'S'
             }
         ],
@@ -58,14 +61,15 @@ def Put_Item(dynamo,tbName):
     for repeat in range(10):
         Designation = ['Software Engineer','Software Quality Engineer', 'Security Analyst']
         Name = ['Christon', 'Wilfred', 'Rohan']
-        SerName = ['Cardoza', 'Fernendes','D\'Souza']
+        Surname = ['Cardoza', 'Fernendes','D\'Souza']
         response = dynamo.put_item(
     
             TableName= tbName,
             Item={
-                'Designation':{'S': random.choice(Designation)} ,
+                 
                 'Name':{'S':random.choice(Name)},
-                'SerName':{'S':random.choice(SerName)}
+                'Surname':{'S':random.choice(Surname)},
+                'Designation':{'S': random.choice(Designation)}
     
     
             }
@@ -79,15 +83,69 @@ def Get_Item(dynamo,tbName):
         TableName = tbName,
 
         Key={
-            'Designation': {
-                'S': 'Software Engineer',
-            },
+            
             'Name': {
-                'S': 'Christon',
+                'S': 'Christon'
+            },
+            'Surname': {
+                'S': 'Cardoza'
             }
-        },
+            
+        }
 
     )
 
     print(response['Item'])
     print(response)
+
+def Delete_Item(dynamo,tbName):
+    response = dynamo.delete_item(
+
+        TableName = tbName,
+
+        Key={
+            
+            'Name': {
+                'S': 'Christon'
+            },
+            'Surname': {
+                'S': 'Cardoza'
+            }
+            
+        }
+
+    )
+
+    # print(response['Item'])
+    print(response)
+    
+def Update_Item(dynamo,tbName):
+    
+    response = dynamo.update_item(
+        TableName = tbName,
+        ExpressionAttributeNames={
+            
+            '#Y': 'Year_of_joining'
+        },
+        UpdateExpression='SET #Y = :y',
+        ExpressionAttributeValues={
+            ':y': {
+                'N': '2020',
+            },
+        },
+        Key={
+            'Name': {
+                    'S': 'Rohan'
+                },
+                'Surname': {
+                    'S': 'D\'Souza'
+                }
+        },
+        ReturnValues='UPDATED_NEW'
+    )
+    
+    print(response)
+
+    
+    
+
